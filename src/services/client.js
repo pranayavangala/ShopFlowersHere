8cf594ae77fd3a9155c067f28e30f3459d8b7b0d
Pranaya <pranayavanagala@gmail.com>
2026-06-11 13:13:38 -0500

    Update backend URL

--- src/services/client.js
+++ src/services/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://pythonbackendapp.onrender.com";
 
 const API = axios.create({
   baseURL: BASE_URL,
